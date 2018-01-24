aec2ada13862b61203d9c6825b454efa5cd7c815
CK Kashyap <ckkashyap@gmail.com>
2018-01-24 06:20:59 -0800

    debug code moved into its own file

--- compiler/bridge.clj
+++ compiler/bridge.clj
@@ -1,15 +1,5 @@
 (ns compiler.bridge)
 
-(def line-number (atom 0))
-(def indent (atom 0))
-
-(defn debug-indent [] (swap! indent #(+ % 4)))
-(defn debug-outdent [] (swap! indent #(- % 4)))
-
-(defn debug-print1 [s]
-  (spit "debug.txt" (str @line-number ": " (clojure.string/join (repeat @indent " ")) s "\n") :append true)
-  (swap! line-number #(inc %)))
-
 (defn car [l] (first l))
 (defn cdr [l] (rest l))
 (defn cddr [l] (rest (rest l)))
